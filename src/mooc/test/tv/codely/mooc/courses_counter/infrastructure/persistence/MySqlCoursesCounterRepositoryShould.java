4f122e93fbfe028052c1a61f72e9a036214d68ba
Jose Manuel Lezcano Fresno <jmlezcano@atsistemas.com>
2022-09-28 20:08:51 +0200

    replace junit4 by junit5 in tests cases

--- src/mooc/test/tv/codely/mooc/courses_counter/infrastructure/persistence/MySqlCoursesCounterRepositoryShould.java
+++ src/mooc/test/tv/codely/mooc/courses_counter/infrastructure/persistence/MySqlCoursesCounterRepositoryShould.java
@@ -8,7 +8,7 @@ import tv.codely.mooc.courses_counter.domain.CoursesCounterMother;
 import javax.transaction.Transactional;
 import java.util.Optional;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Transactional
 class MySqlCoursesCounterRepositoryShould extends CoursesCounterModuleInfrastructureTestCase {
