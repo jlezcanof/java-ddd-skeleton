6cbe95378d85ce64c9cce7e797b2174b9efb211a
Jose Manuel Lezcano Fresno <jmlezcano@atsistemas.com>
2022-10-07 08:54:35 +0200

    bug fixed in repo memory

--- src/backoffice/main/resources/database/backoffice.sql
+++ src/backoffice/main/resources/database/backoffice.sql
@@ -6,9 +6,9 @@ CREATE TABLE IF NOT EXISTS `courses` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `videos` (
-    id   CHAR(36)     NOT NULL,
-    url  VARCHAR(255) NOT NULL,
-    text TEXT         NOT NULL,
-    title VARCHAR(155) NOT NULL,
-    PRIMARY KEY (id)
+    `id`   CHAR(36)     NOT NULL,
+    `url`  VARCHAR(255) NOT NULL,
+    `text` TEXT         NOT NULL,
+    `title` VARCHAR(155) NOT NULL,
+    PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
